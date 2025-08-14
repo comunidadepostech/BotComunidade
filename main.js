ec231991f2141ada700d3771dfe31b6aeb7bd3a0
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2025-08-13 21:00:48 -0300

    Testes /create

--- main.js
+++ main.js
@@ -648,12 +648,19 @@ client.on(Events.InteractionCreate, async interaction => {
                         }
                     });
 
-                    const roles = await interaction.guild.roles.fetch()
+                    const roles = (await interaction.guild.roles.fetch()).map(role => [].includes(role.name)? role.id : null).filter(role => role !== null);
 
+                    console.log(roles)
+                    /*
                     const classCategory = await interaction.guild.channels.create({
                         name: className,
                         type: 4, // Categoria
                         permissionOverwrites: [
+                            {
+                                id: classRole,
+                                allow: classChannels.permissions,
+                                deny: classChannels.permissions_deny
+                            },
                             {
                                 id: interaction.guild.roles.everyone,
                                 deny: [PermissionsBitField.Flags.ViewChannel]
@@ -723,7 +730,7 @@ client.on(Events.InteractionCreate, async interaction => {
                             }
                         })
                     }
-
+                    */
                 } catch (error) {
                     console.error(error);
                     await interaction.editReply({
