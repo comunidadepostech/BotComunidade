73ba7ef97d7f8d73066fc3fa9bc04ab0322bb62e
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2025-08-14 19:48:01 -0300

    Testes banner

--- main.js
+++ main.js
@@ -16,10 +16,9 @@ import {
 import mysql from 'mysql2'
 import {somePermissionsChannels, allPermissionsChannels, classActivations, classChannels} from "./data/classPatterns.mjs"
 import {slashCommands} from "./data/slashCommands.mjs"
-import Canvas from '@napi-rs/canvas'
+import {Canvas, createCanvas, Image} from '@napi-rs/canvas'
 import request  from 'undici'
-import {File, Blob} from "fetch-blob"
-global.File = File;
+import readFile from 'fs/promises'
 
 
 // Define os principais acessos que o Bot precisa para poder funcionar corretamente
@@ -619,28 +618,33 @@ client.on(Events.GuildMemberAdd, async member => {
         };
 
         async function sendWelcome(profile, targetChannel){
-            const canvas = Canvas.createCanvas(1401, 571);
+            const canvas = createCanvas(700, 250);
             const context = canvas.getContext('2d');
-            const background = await Canvas.loadImage('./data/wallpaper.png');
-            context.drawImage(background, 0, 0, canvas.width, canvas.height);
-            const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
 
-            const { body } = await request(profile.displayAvatarURL({ extension: 'jpg' }));
-            const avatar = await Canvas.loadImage(await body.arrayBuffer());
-
-            context.drawImage(avatar, 25, 25, 200, 200);
-            context.beginPath();
-            context.arc(125, 125, 100, 0, Math.PI * 2, true);
-            context.closePath();
-            context.clip();
+            const background = await readFile('./data/wallpaper.jpg');
+            const backgroundImage = new Image();
+            backgroundImage.src = background;
+            context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
             context.font = '28px sans-serif';
             context.fillStyle = '#ffffff';
             context.fillText('Profile', canvas.width / 2.5, canvas.height / 3.5);
 
-            context.font = applyText(canvas, profile.displayName);
+            context.font = applyText(canvas, `${profile.displayName}!`);
             context.fillStyle = '#ffffff';
-            context.fillText(profile.displayName, canvas.width / 2.5, canvas.height / 1.8);
+            context.fillText(`${profile.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+            context.beginPath();
+            context.arc(125, 125, 100, 0, Math.PI * 2, true);
+            context.closePath();
+            context.clip();
+
+            const { body } = await request(profile.displayAvatarURL({ format: 'jpg' }));
+            const avatar = new Image();
+            avatar.src = Buffer.from(await body.arrayBuffer());
+            context.drawImage(avatar, 25, 25, 200, 200);
+
+            const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
 
             targetChannel.send({ files: [attachment] });
         }
