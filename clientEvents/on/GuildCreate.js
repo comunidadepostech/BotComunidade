09335a071ad0bfb9cd3531133d1bb240b8929b9d
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2025-11-25 17:53:07 -0300

    Refatoração do evento `GuildCreate`: substituição de `db` e `flags` por `bot` e simplificação das propriedades e métodos associados.

--- clientEvents/on/GuildCreate.js
+++ clientEvents/on/GuildCreate.js
@@ -1,23 +1,17 @@
 import {Events} from "discord.js";
 
 export class GuildCreate {
-    constructor(db, flags, defaultFlags){
+    constructor(bot){
         this.name = Events.GuildCreate; // O nome do evento
         this.once = false;
-
-        this._db = db;
-        this.flags = flags
-        this.defaultFlags = defaultFlags
+        this.bot = bot
     }
 
-    async execute(client, guild){
-        console.info(`LOG - ${client.user.username} adicionado ao servidor ${guild.name} com ID ${guild.id}`);
+    async execute(guild){
+        console.info(`LOG - ${this.bot.client.user.username} adicionado ao servidor ${guild.name} com ID ${guild.id}`);
 
-        // await guild.commands.set(slashCommands.map(c => c.commandBuild));
-        // console.log(`LOG - Comandos registrados em ${guild.name}`);
-
-        this.flags[guild.id] = this.defaultFlags
-        await this._db.getFlags(guild.id)
+        this.bot.flags[guild.id] = this.bot.defaultFlags
+        await this.bot.db.getFlags(guild.id)
         console.log(`LOG - Flags do servidor ${guild.name} carregadas`);
     }
 }
