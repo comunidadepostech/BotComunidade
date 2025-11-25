0bf73a2fdad5d0f8bca14729f4a6e340c4c113d7
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2025-11-24 23:21:37 -0300

    Adição do evento `MessageUpdate`: coleta de informações sobre enquetes finalizadas, geração de hash da pergunta e envio dos dados para o endpoint N8N.

---
+++ clientEvents/on/MessageUpdate.js
@@ -0,0 +1,52 @@
+import {Events} from "discord.js";
+import crypto from "node:crypto";
+import fetch from "node-fetch";
+
+export class MessageUpdate {
+    constructor(bot){
+        this.name = Events.MessageUpdate;
+        this.once = false;
+        this.bot = bot
+    }
+
+    async execute(interaction){
+        if (!interaction.poll) return
+
+        if (!this.bot.flags[interaction.guild.id]["savePolls"]) return
+
+        if (interaction.poll.resultsFinalized) {
+            const now = Date.now(); // 3 horas antes (GMT-3), momento em que a enquete terminou
+            const expirity = new Date(interaction.poll.expiresTimestamp);
+
+            let className = await this.bot.client.channels.fetch(interaction.channelId);
+            className = className.name
+
+            let guildName = await this.bot.client.guilds.fetch(interaction.guild.id)
+            guildName = guildName.name
+
+            // Prepara o body para ser enviado para o n8n
+            let body = {
+                created_by: interaction.author.globalName,
+                guild: guildName,
+                poll_category: className,
+                poll_hash: crypto.createHash('sha1').update(interaction.poll.question.text).digest('hex'),
+                question: interaction.poll.question.text, // a pergunta da enquete
+                answers: interaction.poll.answers.map(answer => { // lista de respostas
+                    return {response: answer.text, answers: answer.voteCount}
+                }),
+                duration: `${((now - expirity) / 1000 / 60 / 60).toFixed(0)}` // horas
+            };
+
+            //console.debug(JSON.stringify(body, null, 2))
+
+            fetch(process.env.N8N_ENDPOINT + '/salvarEnquete', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': process.env.N8N_TOKEN
+                },
+                body: JSON.stringify(body)
+            })
+        }
+    }
+}
