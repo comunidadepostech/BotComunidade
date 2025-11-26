f4093618377f01bc774f67f7e153b2d21203dc01
Felipe Carvalho <felipesouzacarvalho08@gmail.com>
2025-11-26 07:42:59 -0300

    Adição de nova task `CountMembersByRole` no `scheduler` e uso de `logger` para padronização de logs. Ajuste na execução de tasks com intervalo `0` utilizando `getTimeUntilNextMonth`.

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,16 +1,27 @@
 import CheckGuildsEvents from "./tasks/checkGuildsEvents.js";
+import CountMembersByRole from "./tasks/countMemberByRole.js";
+import logger from "../utils/logger.js";
+import getTimeUntilNextMonth from "../utils/getTimeUntilNextMonth.js";
 
 export default class Scheduler {
     constructor(botInstance) {
         this.tasks = [
-            {instance: new CheckGuildsEvents(botInstance), timeInMinutes: Number(process.env.EVENT_CHECK_TIME)}
-        ];
+            {instance: new CheckGuildsEvents(botInstance), timeInMinutes: Number(process.env.EVENT_CHECK_TIME)},
+            {instance: new CountMembersByRole(botInstance), timeInMinutes: 0}
+        ]
     }
 
     async start() {
         for (const task of this.tasks) {
-            task.instance.execute().catch((error) => console.log(`Erro ao executar ${task.instance.name}: ${error}`)) // Executa cada uma das tasks uma vez para acelerar o debug (pode ser desativado)
-            setInterval(() => task.instance.execute().catch((error) => console.log(`Erro ao executar ${task.instance.name}: ${error}`)), task.timeInMinutes * 60 * 1000);
+            // Executa cada uma das tasks uma vez para acelerar o debug (deve ser desativado)
+            //task.instance.execute().catch((error) => logger.error(`Erro ao executar ${task.instance.name}: ${error}`))
+            task.instance.execute().catch((error) => logger.error(`Erro ao executar ${task.instance.name}: ${error}`))
+
+            if (task.timeInMinutes === 0) {
+                setTimeout(() => task.instance.execute().catch((error) => logger.error(`Erro ao executar ${task.instance.name}: ${error}`)), getTimeUntilNextMonth(task.instance.name))
+            } else {
+                setInterval(() => task.instance.execute().catch((error) => logger.error(`Erro ao executar ${task.instance.name}: ${error}`)), task.timeInMinutes * 60 * 1000);
+            }
         }
     }
 }
